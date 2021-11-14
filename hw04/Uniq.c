4e08675479e12066b37dddb39c947554341dbc14
František Holý <holyfra1@fit.cvut.cz>
2021-11-15 00:41:08 +0100

    HW04: Added exceptions and function to print error message

--- hw04/Uniq.c
+++ hw04/Uniq.c
@@ -6,29 +6,62 @@
 //
 
 bool CheckInput( int returnVal, int wantedVal );
+void ErrorMsg( void );
 
+//
+
+enum keysEnum
+{
+    addKey = '+',
+    calKey = '?',
+    defKey = ' ',
+};
 
 //
 
 int main ( int argc, char * argv [] )
 {
-    char key;
+    char key = ' ';
+    long count = 0;
     printf("Pozadavky:\n");
-    if( CheckInput( scanf("%c", &key ), 1 ) )
+
+    if( CheckInput( scanf( " %c", &key ), 1 ) 
+            || ( key != addKey && key != calKey ) ) {
+        ErrorMsg();
         return EXIT_FAILURE;
+    }
 
+    if( key == addKey ) {
+        int id = 0;
 
+        if( CheckInput( scanf( " %d", &id ), 1 ) 
+                || id < 0 || id > 99999 ) {
+            ErrorMsg();
+            return EXIT_FAILURE;
+        }
 
+    }
+    else if( key == calKey ) {
+        int from = 0;
+        int to = 0;
 
+        if( CheckInput( scanf( " %d %d", &from, &to ), 2 )
+                || from < 0 || to >= count || from > to ) {
+            ErrorMsg();
+            return EXIT_FAILURE;
+        }
+
+    }
 
     return EXIT_SUCCESS;
 }
 
 bool CheckInput( int returnVal, int wantedVal )
 {
-    if( returnVal != wantedVal ) {
-        printf("Nespravny vstup.\n");
-        return true;
-    }
-    return false;
+    return returnVal != wantedVal;
+}
+
+void ErrorMsg( void )
+{
+    printf("Nespravny vstup.\n");
 }
