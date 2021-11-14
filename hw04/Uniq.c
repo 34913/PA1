2d3457c9104d9c597b7df0306d9590ba5d52ec5e
František Holý <holyfra1@fit.cvut.cz>
2021-11-15 00:46:13 +0100

    HW04: Added while loop to run until EOF is on input

--- hw04/Uniq.c
+++ hw04/Uniq.c
@@ -23,34 +23,41 @@ int main ( int argc, char * argv [] )
 {
     char key = ' ';
     long count = 0;
+    int r = 0;
     printf("Pozadavky:\n");
 
-    if( CheckInput( scanf( " %c", &key ), 1 ) 
-            || ( key != addKey && key != calKey ) ) {
+    while( true ) {
+        r = CheckInput( scanf( " %c", &key ), 1 );
+            
+        if( r || ( key != addKey && key != calKey ) )
+            break;
+
+        if( key == addKey ) {
+            int id = 0;
+
+            if( CheckInput( scanf( " %d", &id ), 1 ) 
+                    || id < 0 || id > 99999 ) {
+                ErrorMsg();
+                return EXIT_FAILURE;
+            }
+
+        }
+        else if( key == calKey ) {
+            int from = 0;
+            int to = 0;
+
+            if( CheckInput( scanf( " %d %d", &from, &to ), 2 )
+                    || from < 0 || to >= count || from > to ) {
+                ErrorMsg();
+                return EXIT_FAILURE;
+            }
+
+        }
+    }
+
+    if( r != EOF ) {
         ErrorMsg();
         return EXIT_FAILURE;
-    }
-
-    if( key == addKey ) {
-        int id = 0;
-
-        if( CheckInput( scanf( " %d", &id ), 1 ) 
-                || id < 0 || id > 99999 ) {
-            ErrorMsg();
-            return EXIT_FAILURE;
-        }
-
-    }
-    else if( key == calKey ) {
-        int from = 0;
-        int to = 0;
-
-        if( CheckInput( scanf( " %d %d", &from, &to ), 2 )
-                || from < 0 || to >= count || from > to ) {
-            ErrorMsg();
-            return EXIT_FAILURE;
-        }
-
     }
 
     return EXIT_SUCCESS;
@@ -65,3 +72,4 @@ void ErrorMsg( void )
 {
     printf("Nespravny vstup.\n");
 }
+
