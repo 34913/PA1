1dbf5516a88829fd1c5d6988cd16acd8bae4f33f
František Holý <holyfra1@fit.cvut.cz>
2021-11-15 00:23:50 +0100

    HW04: Added input check function

--- hw04/Uniq.c
+++ hw04/Uniq.c
@@ -3,8 +3,32 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+//
+
+bool CheckInput( int returnVal, int wantedVal );
+
+
+//
+
 int main ( int argc, char * argv [] )
 {
-    
-    return 0;
+    char key;
+    printf("Pozadavky:\n");
+    if( CheckInput( scanf("%c", &key ), 1 ) )
+        return EXIT_FAILURE;
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
+
+bool CheckInput( int returnVal, int wantedVal )
+{
+    if( returnVal != wantedVal ) {
+        printf("Nespravny vstup.\n");
+        return true;
+    }
+    return false;
 }
