7908b3b2c7c1cc0295bcf6b5a289331edf862a02
František Holý <holyfra1@fit.cvut.cz>
2021-11-15 15:03:35 +0100

    HW04: AddUser function, expanding the array and counting the number of users with same id

--- hw04/Uniq.c
+++ hw04/Uniq.c
@@ -3,12 +3,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-//
-
-bool CheckInput     ( int returnVal, int wantedVal );
-void ErrorMsg       ( void );
-int  AddUser        ( int id, database *db );
-int  CalculateUsers ( int from, int to, database *db );
 
 //
 
@@ -19,20 +13,29 @@ enum keysEnum
     defKey = ' ',
 };
 
-typedef struct 
+typedef struct dbStruct
 {
     int *data;
     int len;
 } database;
+
+
+//
+
+bool CheckInput     ( int returnVal, int wantedVal );
+void ErrorMsg       ( void );
+int  AddUser        ( int id, database *db );
+int  CalculateUsers ( int from, int to, database *db );
 
 //
 
 int main ( int argc, char * argv [] )
 {
     char key        = defKey;
-    long count      = 0;
     int r           = 0;
     database db;
+    db.len = 0;
+    db.data = NULL;
 
     printf("Pozadavky:\n");
 
@@ -60,13 +63,13 @@ int main ( int argc, char * argv [] )
             int to = 0;
 
             if( CheckInput( scanf( " %d %d", &from, &to ), 2 )
-                    || from < 0 || to >= count || from > to ) {
+                    || from < 0 || to >= db.len || from > to ) {
                 free( db.data );
                 ErrorMsg();
                 return EXIT_FAILURE;
             }
 
-            pritnf("%d / %d\n", CalculateUsers( from, to, &db ), to - from + 1 );
+            printf("%d / %d\n", CalculateUsers( from, to, &db ), to - from + 1 );
             
         }
     }
@@ -92,10 +95,43 @@ void ErrorMsg( void )
 
 int AddUser( int id, database *db )
 {
+    void *help;
+    if( db->len == 0 )
+        help = malloc( sizeof( int ) );
+    else
+        help = realloc( db->data, sizeof( int ) * (db->len + 1) );
 
+    if( help == NULL ) {
+        printf( "Not enough memory\n" );
+        free( db->data );
+        exit( EXIT_FAILURE );
+    }
+
+    db->data = (int* )help;
+    db->data[db->len] = id;
+
+    db->len = db->len + 1;
+
+    int count = 1;
+    for( int i = 0; i < db->len - 1; i++ ) {
+        if( (db->data)[i] == id )
+            count++;
+    }
+    return count;
 }
 
 int CalculateUsers( int from, int to, database *db )
 {
+    int count = db->len;
 
+    for( int i = 0; i < db->len; i++ ) {
+        for( int y = i + 1; y < db->len; y++ ) {
+            if( db->data[i] == db->data[y] ) {
+                count--;
+                break;
+            }
+        }
+    }
+
+    return count;
 }
