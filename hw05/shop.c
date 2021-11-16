5487e384507b0e522fbeb4390deaa9989939df5e
František Holý <holyfra1@fit.cvut.cz>
2021-11-17 00:16:26 +0100

    HW05: Added key chars and their branches in switch for each command

--- hw05/shop.c
+++ hw05/shop.c
@@ -4,15 +4,49 @@
 #include <stdbool.h>    // boolean values
 #include <stdlib.h>     // malloc
 
-
+enum keys {
+    add = '+',
+    all = '#',
+    num = '?',
+    def = ' ',
+};
 
 void ErrorMessage();
 
 int main(void)
 {
+    // saying how many of tracked things are going to be displayed
+    int tracked = 0;
 
-    return 0;
+    if( scanf( "%d", &tracked ) != 1 || tracked <= 0 ) {
+        ErrorMessage();
+        return EXIT_FAILURE;
+    }
 
+    while( true ) {
+        char c = def;
+        if( scanf( "%c", &c ) != 1 ) {
+            ErrorMessage();
+            return EXIT_FAILURE;
+        }
+
+        switch ( c ) {
+            case add:
+
+                break;
+            case all:
+
+                break;
+            case num:
+
+                break;
+            default:
+                ErrorMessage();
+                return EXIT_FAILURE;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
 
 void ErrorMessage() {
