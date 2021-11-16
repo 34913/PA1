583ceee1332e1ae3c7a935e38f64b3948c884691
František Holý <holyfra1@fit.cvut.cz>
2021-11-16 23:59:24 +0100

    HW05: Added error message printing function

--- hw05/shop.c
+++ hw05/shop.c
@@ -4,9 +4,17 @@
 #include <stdbool.h>    // boolean values
 #include <stdlib.h>     // malloc
 
+
+
+void ErrorMessage();
+
 int main(void)
 {
-    
+
     return 0;
+
 }
 
+void ErrorMessage() {
+    printf( "Nespravny vstup.\n");
+}
