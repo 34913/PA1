01ac72bbea38ea5eb6569fc8f5c45dd6b516c4c2
František Holý <holyfra1@fit.cvut.cz>
2021-11-19 12:13:55 +0100

    HW05: Overall revised pointers to data, had to be done at once

--- hw05/shop.c
+++ hw05/shop.c
@@ -16,11 +16,16 @@ enum keys {
 
 typedef struct record_struct
 {
-    char **names;
-    int *counts;
+    char *name;
+    long count;
+} record;
+
+typedef struct list_struct
+{
+    record **names;
     int nameLen;
     long length;
-} record;
+} list;
 
 typedef struct pointRecord_struct
 {
@@ -29,8 +34,8 @@ typedef struct pointRecord_struct
 } pointRecord;
 
 void ErrorMessage();
-void ClearArr( record *arr, pointRecord *sorted );
-bool ExtendArr ( record *arr, pointRecord *sorted );
+void ClearArr( list *arr, pointRecord *sorted );
+bool ExtendArr ( list *arr, pointRecord *sorted );
 
 //
 
@@ -39,7 +44,7 @@ int main(void)
     // saying how many of tracked things are going to be displayed
     int tracked = 0;
 
-    record arr[MAX_LEN];
+    list arr[MAX_LEN];
     pointRecord sorted;
     sorted.count = 0;
 
@@ -47,71 +52,79 @@ int main(void)
         arr[i].length = 0;
         arr[i].nameLen = i + 1;
         
-        arr[i].counts = NULL;
         arr[i].names = NULL;
     }
 
-    if( scanf( "%d", &tracked ) != 1 || tracked <= 0 ) {
+    if( scanf( " %d", &tracked ) != 1 || tracked <= 0 ) {
         ErrorMessage();
         ClearArr( arr, &sorted );
         return EXIT_FAILURE;
     }
 
-    while( true ) {
-        char c = def;
-        if( scanf( "%c", &c ) != 1 ) {
+    char c = def;
+    int d = scanf( " %c", &c );
+    while( d == 1 ) {
+        
+        if( c == add ) {
+            char str[ MAX_LEN ];
+            
+            if( scanf( " %99s", str ) != 1 ) {
+                ErrorMessage();
+                ClearArr( arr, &sorted );
+                return EXIT_FAILURE;
+            }
+
+            int len = strlen( str );
+            if( len == MAX_LEN ) {
+                if( getchar() != '\n' ) {
+                    ErrorMessage();
+                    ClearArr( arr, &sorted );
+                    return EXIT_FAILURE;
+                }
+            }
+
+            bool exists = false;
+            list *r = &arr[ len - 1 ];
+
+            for( int i = 0; i < r->length; i++ ) {
+                if( strcmp( str, r->names[ i ]->name ) == 0 ) {
+                    exists = true;
+                    r->names[ i ]->count++;
+                    break;
+                }
+            }
+
+            if( !exists ) {
+                if( !ExtendArr( r, &sorted ) ) {
+                    ErrorMessage();
+                    ClearArr( arr, &sorted );
+                    return EXIT_FAILURE;
+                }
+                
+                strncpy( r->names[ r->length - 1 ]->name, str, len );
+                r->names[ r->length - 1 ]->count = 1;
+            }
+        }
+        else if( c == all ) {
+            
+        }
+        else if( c == num ) {
+        
+        }
+        else {
             ErrorMessage();
             ClearArr( arr, &sorted );
             return EXIT_FAILURE;
         }
 
-        switch ( c ) {
-            case add:
-                int strLen;
-                char str[ MAX_LEN ];
-                for ( strLen = 0; strLen < MAX_LEN; strLen++ ) {
-                    str[ strLen ] = getchar();
-                    if( str[ strLen ] == '\n' )
-                        break;
-                }
+        d = scanf( " %c", &c );
+    }
+    ClearArr( arr, &sorted );
 
-                if( strLen == MAX_LEN ) {
-                    if( getchar() != '\n' ) {
-                        ErrorMessage();
-                        ClearArr( arr, &sorted );
-                        return EXIT_FAILURE;
-                    }
-                }
-
-                bool exist = false;
-                record *r = &arr[ strLen - 1 ];
-
-                for( int i = 0; i < r->length; i++ ) {
-                    if( strcmp( str, r->names[ i ] ) == 0 ) {
-                        exist = true;
-                        r->counts++;
-                        break;
-                    }
-                }
-
-                if( !exist ) {
-                    ExtendArr( r, &sorted );
-                    strncpy( &r->names[ r->length - 1 ], &str, strLen);
-                    r->counts[ r->length - 1 ] = 1;
-
-                    sorted.pointers[ sorted.count - 1 ] = r->names[ r->length - 1 ];
-                }
-                break;
-            case all:
-
-                break;
-            case num:
-
-                break;
-            default:
-                ErrorMessage();
-                return EXIT_FAILURE;
-        }
+    if( d != EOF ) {
+        ErrorMessage();
+        ClearArr( arr, &sorted );
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
@@ -122,11 +135,12 @@ void ErrorMessage()
     printf( "Nespravny vstup.\n");
 }
 
-void ClearArr( record *arr, pointRecord *sorted )
+void ClearArr( list *arr, pointRecord *sorted )
 {
     for( int i = 0; i < MAX_LEN; i++ ) {
         for( int y = 0; y < arr[i].length; y++ ) {
-            free( arr[i].names[y] );
+            free( arr[ i ].names[ y ]->name );
+            free( arr[ i ].names[ y ] );
         }
         free( arr[i].names );
     }
@@ -134,22 +148,31 @@ void ClearArr( record *arr, pointRecord *sorted )
     free( sorted->pointers );
 }
 
-bool ExtendArr ( record *arr, pointRecord *sorted )
+bool ExtendArr ( list *r, pointRecord *sorted )
 {
-    void** help;
-    if( arr->length++ == 0 )
-        help = malloc( sizeof( char* ) );
+    void *help1 = NULL;
+    if( r->length++ == 0 )
+        help1 = malloc( sizeof( record* ) );
     else
-        help = realloc( arr->names, sizeof( char * ) * arr->length );
+        help1 = realloc( r->names, sizeof( record* ) * r->length );
+
+    if( help1 == NULL )
+        return false;
+    r->names = ( record** )help1;
     
-    help[arr->length - 1] = malloc( sizeof( char ) * arr->nameLen );
-    arr->names = (char **) help;
+    record *temp = ( record* )malloc( sizeof( record ) );
+    temp->name = ( char* )malloc( sizeof( char ) * r->nameLen );
+    r->names[ r->length - 1 ] = temp;
 
     if( sorted->count++ == 0 )
-        help = malloc( sizeof( record* ) );
+        help1 = malloc( sizeof( record* ) );
     else
-        help = realloc( sorted->pointers, sizeof( record *) * sorted->count );
-    sorted->pointers = (record **) help;
+        help1 = realloc( sorted->pointers, sizeof( record* ) * sorted->count );
+
+    if( help1 == NULL )
+        return false;
+    sorted->pointers = ( record** )help1;
+    sorted->pointers[ sorted->count - 1 ] = temp;
 
     return true;
 }
