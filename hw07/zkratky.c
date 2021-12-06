081a59a0c63d451a039003ff94833dd52463e90c
František Holý <holyfra1@fit.cvut.cz>
2021-12-06 16:32:54 +0100

    HW07: Initial commit of HW07

---
+++ hw07/zkratky.c
@@ -0,0 +1,14 @@
+#include <stdio.h>      // standard I/O
+#include <stdlib.h>     // malloc, realloc, free
+#include <stdbool.h>    // boolean true false logic values
+
+
+
+int main( void )
+{
+
+
+
+    return EXIT_SUCCESS;
+}
+
