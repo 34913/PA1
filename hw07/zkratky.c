2c33c14398ba32206ca0eb71a97973e65002f793
František Holý <holyfra1@fit.cvut.cz>
2021-12-06 17:29:19 +0100

    HW07: Initial reading input

--- hw07/zkratky.c
+++ hw07/zkratky.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>     // malloc, realloc, free
 #include <stdbool.h>    // boolean true false logic values
 
+// using numeric type on all numbers
+#define type long long
+
+//
+
+typedef struct record_struct
+{
+    char* str;
+    type len;
+} record;
+
 //
 
 /**
@@ -14,8 +25,40 @@ void PrintError( void );
 
 int main( void )
 {
-    
+    record wanted;
+    wanted.str      = NULL;
+    wanted.len      = 0;
 
+    //
+
+    printf( "Zkratka:\n" );
+    type count = 0;
+
+    if( ( count = getline( &wanted.str, &wanted.len, stdin ) ) == -1 ) {
+        PrintError();
+        return EXIT_FAILURE;
+    }
+    wanted.str[ count - 1 ] = '\0';
+    wanted.len = count - 1;
+
+    printf( "Problemy:\n" );
+
+    int check;
+    char key;
+    while( ( check = scanf( "%c %d ", &key, &count ) ) == 2 ) {
+
+
+
+
+
+    }
+
+    //
+
+    if( check != EOF ) {
+        PrintError();
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
