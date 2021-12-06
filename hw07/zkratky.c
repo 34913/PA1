f63e91b175e1aaeb70670a7d6786d3e5f6c8909b
František Holý <holyfra1@fit.cvut.cz>
2021-12-06 16:38:06 +0100

    HW07: Error printing function

--- hw07/zkratky.c
+++ hw07/zkratky.c
@@ -2,13 +2,27 @@
 #include <stdlib.h>     // malloc, realloc, free
 #include <stdbool.h>    // boolean true false logic values
 
+//
 
+/**
+ * @brief       Prints common error message on stdout
+ * 
+ */
+void PrintError( void );
+
+//
 
 int main( void )
 {
-
+    
 
 
     return EXIT_SUCCESS;
 }
 
+//
+
+void PrintError( void )
+{
+    printf( "Nespravny vstup.\n" );    
+}
