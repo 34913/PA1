0596c8ebed9ef7f6f19bfc535d0bef67ed599c46
František Holý <holyfra1@fit.cvut.cz>
2021-12-11 18:18:26 +0100

    HW07: Printing out the outcome

--- hw07/zkratky.c
+++ hw07/zkratky.c
@@ -341,7 +341,29 @@ int main( void )
 
         //
 
-        
+        if( rec.compSize.len > 0 ) {
+            for( type i = 0; i < rec.max; i++ )
+                rec.comp[ 0 ]->arr[ i ]->data
+                    = toupper( rec.comp[ 0 ]->arr[ i ]->data );
+            for( type y = 0; y < str.size.len; y++ )
+                printf( "%c", str.arr[ y ]->data );
+            printf( "\n" );
+            
+            for( type i = 1; i < rec.compSize.len; i++ ) {
+                for( type y = 0; y < rec.max; y++ )
+                    rec.comp[ i - 1 ]->arr[ y ]->data
+                        = tolower( rec.comp[ i - 1 ]->arr[ y ]->data );
+                for( type y = 0; y < rec.max; y++ )
+                    rec.comp[ i ]->arr[ y ]->data
+                        = toupper( rec.comp[ i ]->arr[ y ]->data );
+
+                for( type y = 0; y < str.size.len; y++ )
+                    printf( "%c", str.arr[ y ]->data );
+                printf( "\n" );
+            }
+        }
+
+        printf( "%lld\n", rec.compSize.len );
 
         //
 
